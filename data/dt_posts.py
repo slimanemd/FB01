4e9d64a1d9df8ae3ebb6fb5701d32afb0e03a565
slimanemd <slimanemd@hotmail.com>
2020-04-08 13:41:53 +0100

    add data posts , title, user to views home + about

---
+++ data/dt_posts.py
@@ -0,0 +1,18 @@
+#dummy data
+posts = [
+  {
+    'author' : 'Ali',
+    'title' : 'Blog',
+    'content' : 'First',
+    'date_posted': 'April 20, 2018'
+  },
+  {
+    'author' : 'Ben',
+    'title' : 'Post',
+    'content' : 'Second',
+    'date_posted': 'May 12, 2018'
+  }
+]
+
+user =  "SLIMANE"
+title =  "My Blog"
--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,17 @@
+from data.dt_posts import *
 from flask import Flask, render_template
 app = Flask(__name__)
 
 #home route
 @app.route('/')
 @app.route('/home')
-def home():
-  return  render_template('home.html') #'<h1>Hello World</h1>'
+def home():  #, 
+  return render_template('home.html', data = {'user':user, 'posts': posts}) #'<h1>Hello World</h1>'
 
 #about route
 @app.route('/about')
 def about():
-  return render_template('about.html') #'<h1>About Page</h1>'  
+  return render_template('about.html',data= {'title':title}) #'<h1>About Page</h1>'  
+
+if __name__ == "__main__":
+    app.run(debug=True)
