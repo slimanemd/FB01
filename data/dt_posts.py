95d1ac6c27aaa85a213ef256743908da4bb51393
slimanemd <slimanemd@hotmail.com>
2020-04-09 23:16:25 +0100

    integarte forms & validation

--- data/dt_posts.py
+++ data/dt_posts.py
@@ -2,16 +2,24 @@
 posts = [
   {
     'author' : 'Ali',
-    'title' : 'Blog',
-    'content' : 'First',
-    'date_posted': 'April 20, 2018'
+    'title' : 'Bl<h1>og',
+    'content' : 'First  jfjhsjkdfh kjsdhjk f hkjfh kjsdhf kjsdhfkjh sdkfhkj sdhkjfh ksdjhfjkh',
+    'date_posted': 'April 20, 2018',
+    'author_image_file': 'me.png'
+
   },
   {
     'author' : 'Ben',
     'title' : 'Post',
-    'content' : 'Second',
-    'date_posted': 'May 12, 2018'
-  }
+    'content' : 'Second kdjhf jkshjfh ksdjhfjk sdkjh fkjsdh kjfhsdkj fhkjsdkh fjkhsdjkhf kjsdh fkjsdh kjfhkjsdhf kjsdhfjk',
+    'date_posted': 'May 12, 2018',
+  },
+  {
+    'author' : 'sol',
+    'title' : 'Dost',
+    'content' : 'Third kkf jksdfjk kjsd kjfh jksdh fjkhjkfh jksdh fkjhsdjkfh kjsdhkjf hksdjh fkjsdhjkf hkjsd',
+    'date_posted': 'June 22, 2018',
+  }  
 ]
 
 user =  "SLIMANE"
